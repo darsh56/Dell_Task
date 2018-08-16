a2183a8e4ef273f8fd5fbf63898be9982b254388
darsh56 <darshparikh56@gmail.com>
2018-08-16 13:24:05 -0500

    Scroll API
    Fetch any number of data from Cisco switch and store them in Elastic search

---
+++ 93180-f.py
@@ -0,0 +1,65 @@
+import ciscodata
+import logging
+import argparse
+from logging.handlers import RotatingFileHandler
+import sys
+import time
+
+#####################################################
+# main 
+#####################################################
+if __name__ == "__main__":
+   
+    my_handler = RotatingFileHandler(filename='./cisco_data.log',
+                                     mode='a',
+                                     maxBytes=1*1024*1024,  # 1 MB
+                                     backupCount=10,
+                                     encoding=None,
+                                     delay=0)
+
+    log_level = logging.DEBUG
+    app_log = logging.getLogger()
+    my_handler.setLevel(log_level)
+    formatter = logging.Formatter('%(asctime)s %(levelname)s,%(message)s',
+                                              datefmt='%b %d,%Y %H:%M:%S')
+    my_handler.setFormatter(formatter)
+    app_log.setLevel(log_level)
+    app_log.addHandler(my_handler)
+    #
+    std_out = logging.StreamHandler(sys.stdout)
+    std_out.setLevel(log_level)
+    std_out.setFormatter(formatter)
+    app_log.addHandler(std_out)
+    #
+    logging.info("Starting cisco_switch_data")
+    logging.info("log level: {}".format(logging.getLevelName(log_level)))
+    #
+   
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--init', type=bool, help='reinitialize the data index', required=True)
+    parser.add_argument('--ip', type=str, help='elasticsearch ip default: 100.80.97.105', required=True)
+    parser.add_argument('--port', type=int, help='elasticsearch port default: 9200', required=True)
+    parser.add_argument('--switchip', type=str, help='switch ip default: 100.80.0.130', required=True)    
+    parser.add_argument('--username', help='switch username default: admin', required=True)
+    parser.add_argument('--password', help='switch password default: Password.1', required=True)
+    parser.add_argument('--loop', type=bool, help='run script continuously (every 5 seconds)', required=True)
+    
+    #########################################
+
+    args, unknown = parser.parse_known_args()
+
+    model_93180_obj = ciscodata.model_93180(args.ip, args.port, args.switchip, args.username, args.password)
+
+    if args.init:
+        model_93180_obj.create_index()
+
+    if args.loop:
+        while True:
+          interface_info=model_93180_obj.switch_interface()
+          version_info=model_93180_obj.switch_version()
+          manufacturer,SwitchID,host_name,Time=model_93180_obj.parse_version_data(version_info)
+          InCount,OutCount,eth_speed,PortNumber=model_93180_obj.parse_interface_data(interface_info)
+          model_93180_obj.store_data(InCount,OutCount,eth_speed,PortNumber,manufacturer,SwitchID,host_name,Time)
+          model_93180_obj.get_data()
+          time.sleep(10)
+   
---
+++ ciscodata-f.py
@@ -0,0 +1,168 @@
+import requests
+import json
+from elasticsearch import Elasticsearch
+import datetime
+import pytz
+import logging
+import re
+import time
+
+
+
+class model_93180:
+	
+    def __init__(self, host, port, switchip, username, password):
+        self.database = "cisco_switch_data"
+        self.table = "switch_counts"
+        self.host = host
+        self.port = port
+        self.switchip=switchip
+        self.username = username
+        self.password = password
+        self.es = Elasticsearch([{'host': self.host, 'port': self.port}])
+    
+    #index is created
+    def create_index(self):
+        logging.debug("Enter create_index")
+        #
+        if self.es.indices.exists(self.database):
+            logging.info("Removing index {index} to recreate".format(index=self.database))
+            self.es.indices.delete(index=self.database)
+        self.es.indices.create(index=self.database)
+
+    #API call show inteface
+    def switch_interface(self):
+    	
+      url='http://{switch_ip}/ins'.format(switch_ip=self.switchip)	 
+      switchuser=self.username
+      switchpassword=self.password
+      myheaders={'content-type':'application/json'}
+      payload={
+       "ins_api": {
+        "version": "1.0",
+        "type": "cli_show",
+        "chunk": "0",
+        "sid": "1",
+        "input": "show interface",
+        "output_format": "json"
+        }
+      }
+      interface_info = requests.post(url,data=json.dumps(payload), headers=myheaders,auth=(switchuser,switchpassword)).json()
+      return interface_info
+
+    
+    #API call show version
+    def switch_version(self):
+    
+      
+      url='http://{switch_ip}/ins'.format(switch_ip=self.switchip)	   
+      switchuser=self.username
+      switchpassword=self.password
+      myheaders={'content-type':'application/json'}
+      payload={
+       "ins_api": {
+       "version": "1.0",
+       "type": "cli_show",
+       "chunk": "0",
+       "sid": "1",
+       "input": "show version",
+       "output_format": "json"
+       }
+      }
+      version_info = requests.post(url,data=json.dumps(payload), headers=myheaders,auth=(switchuser,switchpassword)).json()
+      return version_info
+
+    def parse_version_data(self,version_info):
+
+      
+      collection_time = datetime.datetime.now(pytz.utc)
+      data=version_info['ins_api']['outputs']['output']['body']
+      Time=collection_time
+      manufacturer=data['manufacturer']
+      SwitchID=data['chassis_id']
+      host_name=data['host_name']
+   
+         
+      return manufacturer,SwitchID,host_name,Time            
+        
+    
+    def parse_interface_data(self,interface_info):
+
+      InCount=[]
+      OutCount=[]
+      eth_speed=[]
+      PortNumber=[]
+   
+      pattern = re.compile( r'(\w*)\/(\d*)')
+          
+      
+      for data in interface_info['ins_api']['outputs']['output']['body']['TABLE_interface']['ROW_interface']:
+       if 'eth_inpkts' in data.keys():
+        if data['eth_inpkts'] is not 0:
+         matches= pattern.finditer(data['interface'])
+         for match in matches:
+           name=match.group(1)
+           if "Ethernet" in name:
+             InCount.append(data['eth_inpkts'])
+             OutCount.append(data['eth_outpkts'])
+             eth_speed.append(data['eth_speed'])
+             PortNumber.append(match.group(2))
+                        
+
+
+      return InCount,OutCount,eth_speed,PortNumber
+
+
+
+   
+    def store_data(self,InCount,OutCount,eth_speed,PortNumber,manufacturer,SwitchID,host_name,Time):
+
+      cisco_data = {}
+      cisco_data['manufacturer']=manufacturer
+      cisco_data['SwitchID']=SwitchID
+      cisco_data['host_name']=host_name
+
+      cisco_data['Time'] = Time
+
+      for i in range(len(InCount)):
+  
+        cisco_data['In-Count'] = InCount[i]
+        cisco_data['Out-Count'] = OutCount[i]
+        cisco_data['eth_speed'] = eth_speed[i]
+        cisco_data['Port-Number'] = PortNumber[i] 
+        self.es.index(index=self.database, doc_type=self.table, body=cisco_data)
+
+
+  
+    def get_data(self):
+              
+        result = self.es.search(index="cisco_switch_data",doc_type = "switch_counts", scroll="2m", body= {"size" : 10000,"query" :{"match_all": {}}})
+        sid= result['_scroll_id']
+        scroll_size = len( result['hits']['hits'])
+        data = result['hits']['hits']
+        self.print_data(data)
+
+
+        while scroll_size > 0:
+              #"Scrolling..."
+            result = self.es.scroll(scroll_id=sid, scroll='2m')
+            data = result['hits']['hits']
+            sid= result['_scroll_id']
+            scroll_size = len(data)
+            self.print_data(data)
+
+
+       
+    def print_data(self,data):
+    	for item in data:
+                original_data = item['_source']
+                logging.debug("switch: \'{switch}\' port: {port} incount: {incount} outcount: {outcount} eth_speed: {eth_speed} time: {time} host_name: {host_name} manufacturer: {manufacturer}".
+                              format(time=original_data['Time'],
+                                     switch=original_data['SwitchID'],
+                                     port=original_data['Port-Number'],
+                                     incount=original_data['In-Count'],
+                                     outcount=original_data['Out-Count'],
+                                     eth_speed=original_data['eth_speed'],
+                                     host_name=original_data['host_name'],
+                                     manufacturer=original_data['manufacturer'],
+                              ))   
